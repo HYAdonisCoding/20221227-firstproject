9a648796aab872b4450da8554fb51142d6eef108
Eason <296786475@qq.com>
2023-02-03 17:56:58 +0800

    config count down

--- yanxuan/js/time.js
+++ yanxuan/js/time.js
@@ -5,10 +5,21 @@ function todayFunc(){
 
     var node = document.getElementsByClassName("limit")[0];
     var details = node.getElementsByClassName("time-detail")[0];
-    
-    setHourTime(details.getElementsByClassName("hour")[0], date.getHours());
-    setMinutesTime(details.getElementsByClassName("minute")[0], date.getMinutes());
-    setSecondTime(details.getElementsByClassName("second")[0], date.getSeconds());
+    // 设置到期时间
+    var limitDate = new Date();
+    limitDate.setHours(19);
+    limitDate.setMinutes(0);
+    limitDate.setSeconds(0);
+
+    var lefttime = (limitDate - date) ;
+    var days=Math.floor(lefttime/1000/60/60/24);  //计算天数   1s=1000毫秒
+    var hours=Math.floor(lefttime/1000/60/60%24);
+    var mins=Math.floor(lefttime/1000/60%60);
+    var miao=Math.floor(lefttime/1000%60);
+
+    setHourTime(details.getElementsByClassName("hour")[0], hours);
+    setMinutesTime(details.getElementsByClassName("minute")[0], mins);
+    setSecondTime(details.getElementsByClassName("second")[0], miao);
 }
 
 function setHourTime(detail, time) {
